3e794558f3ea8c5376705b2a04055902e03ab6bc
Lorraine Gaudio <lorraine.gaudio@gmail.com>
2025-08-16 12:00:50 -0600

    Edited lessons 1 through 3 for consistancy

---
+++ Icons.R
@@ -0,0 +1,37 @@
+✔️ Nice!
+  
+👀 Check-in: or What happened?
+  
+🚀 Explore and Play:
+  
+🎯 
+
+⚡ Explain
+
+🔍 Look deeper: 
+
+⚠️
+
+🧹
+
+🗣 Comment
+
+⚖️
+
+🕰
+
+💡
+
+🔗 Link
+
+🧠 Challenge:
+
+🎉
+
+📍 Identify
+
+🛠 Break Things!
+  
+📊 Group
+
+📦 Pack/ Unpack
